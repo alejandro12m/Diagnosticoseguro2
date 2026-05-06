dce7f9d853a16f4145657c4029b3303af068704b
ale12GM <garzonale3@gmail.com>
2026-05-06 03:13:23 -0400

    Subiendo proyecto completo20

--- Core/DTOs/ConsultasDTOs/ResultadoEspecificoDTO.cs
+++ Core/DTOs/ConsultasDTOs/ResultadoEspecificoDTO.cs
@@ -6,7 +6,7 @@
         public string OrdenCodigo { get; set; }
         public string PacienteNombre { get; set; } 
         public string Examen { get; set; }
-        public int Parametro { get; set; }
+        public string Parametro { get; set; }
         public decimal Valor { get; set; }
         public string Unidad { get; set; }
         public string Referencia { get; set; }
