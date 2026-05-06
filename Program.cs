56390c6da71d15eddf88cd8ac59b42ae1eb4d033
ale12GM <garzonale3@gmail.com>
2026-05-05 23:18:57 -0400

    Subiendo proyecto completo7

--- Program.cs
+++ Program.cs
@@ -1,37 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using DiagnosticoMedico.Data;
+﻿using DiagnosticoMedico.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var connectionString = builder.Configuration.GetConnectionString("DiagnosticoMedicoContext")
     ?? Environment.GetEnvironmentVariable("ConnectionStrings__DiagnosticoMedicoContext");
 
 builder.Services.AddDbContext<DiagnosticoMedicoContext>(options =>
     options.UseNpgsql(connectionString));
-// Add services to the container.
+
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
-// Cambia 8080 por 10000
+
+// ✅ PUERTO CORRECTO
 var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+app.Urls.Add($"http://0.0.0.0:{port}");
+
+// ✅ BASE DE DATOS (temporal)
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<DiagnosticoMedicoContext>();
     context.Database.EnsureCreated();
 }
-// Configure the HTTP request pipeline.
 
+// ✅ Swagger SIEMPRE activo
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
-// app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
